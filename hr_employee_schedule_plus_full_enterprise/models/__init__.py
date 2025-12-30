c5b96a0dabbc2c60a8a43f94a943fd81226c3891
ridwan1379 <taryakayahya@gmail.com>
2025-12-30 08:23:01 +0700

    Delete hr_employee_schedule_plus_full_enterprise directory

--- hr_employee_schedule_plus_full_enterprise/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import hr_shift
-from . import hr_employee
-from . import hr_shift_history
-from . import shift_rotation
-from . import res_config_settings
--- hr_employee_schedule_plus_full_enterprise/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import wizard_bulk_assign_shift
-from . import wizard_import_shifts
